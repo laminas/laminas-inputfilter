7ae894af002a5dc031bee1860481f675370f3359
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:12:30 -0600

    Rewriting as Laminas Project package

--- test/InputFilterManagerTest.php
+++ test/InputFilterManagerTest.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-inputfilter for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-inputfilter/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-inputfilter/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\InputFilter;
+namespace LaminasTest\InputFilter;
 
-use Zend\InputFilter\InputFilterPluginManager;
+use Laminas\InputFilter\InputFilterPluginManager;
 
 /**
- * @group Zend_Stdlib
+ * @group Laminas_Stdlib
  */
 class InputFilterManagerTest extends \PHPUnit_Framework_TestCase
 {
@@ -28,33 +27,33 @@ class InputFilterManagerTest extends \PHPUnit_Framework_TestCase
 
     public function testRegisteringInvalidElementRaisesException()
     {
-        $this->setExpectedException('Zend\InputFilter\Exception\RuntimeException');
+        $this->setExpectedException('Laminas\InputFilter\Exception\RuntimeException');
         $this->manager->setService('test', $this);
     }
 
     public function testLoadingInvalidElementRaisesException()
     {
         $this->manager->setInvokableClass('test', get_class($this));
-        $this->setExpectedException('Zend\InputFilter\Exception\RuntimeException');
+        $this->setExpectedException('Laminas\InputFilter\Exception\RuntimeException');
         $this->manager->get('test');
     }
 
     /**
-     * @covers Zend\InputFilter\InputFilterPluginManager::validatePlugin
+     * @covers Laminas\InputFilter\InputFilterPluginManager::validatePlugin
      */
     public function testAllowLoadingInstancesOfInputFilterInterface()
     {
-        $inputFilter = $this->getMock('Zend\InputFilter\InputFilterInterface');
+        $inputFilter = $this->getMock('Laminas\InputFilter\InputFilterInterface');
 
         $this->assertNull($this->manager->validatePlugin($inputFilter));
     }
 
     /**
-     * @covers Zend\InputFilter\InputFilterPluginManager::validatePlugin
+     * @covers Laminas\InputFilter\InputFilterPluginManager::validatePlugin
      */
     public function testAllowLoadingInstancesOfInputInterface()
     {
-        $input = $this->getMock('Zend\InputFilter\InputInterface');
+        $input = $this->getMock('Laminas\InputFilter\InputInterface');
 
         $this->assertNull($this->manager->validatePlugin($input));
     }
