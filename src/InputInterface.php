cc6d0e0dbc00bbf8d9306532f1185a5842d74cc6
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:11:41 -0600

    Rewriting as Laminas Project package

--- src/InputInterface.php
+++ src/InputInterface.php
@@ -1,21 +1,19 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_InputFilter
+ * @see       https://github.com/laminas/laminas-inputfilter for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-inputfilter/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-inputfilter/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\InputFilter;
+namespace Laminas\InputFilter;
 
-use Zend\Filter\FilterChain;
-use Zend\Validator\ValidatorChain;
+use Laminas\Filter\FilterChain;
+use Laminas\Validator\ValidatorChain;
 
 /**
- * @category   Zend
- * @package    Zend_InputFilter
+ * @category   Laminas
+ * @package    Laminas_InputFilter
  */
 interface InputInterface
 {
