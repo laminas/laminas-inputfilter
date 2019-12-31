93e689272d4ba0753ea81cbe4c06c06c34392e30
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:13:23 -0600

    Rewriting as Laminas Project package

--- test/OptionalInputFilterTest.php
+++ test/OptionalInputFilterTest.php
@@ -1,23 +1,22 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-inputfilter for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-inputfilter/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-inputfilter/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\InputFilter;
+namespace LaminasTest\InputFilter;
 
 use ArrayIterator;
-use Zend\InputFilter\Input;
-use Zend\InputFilter\InputFilter;
-use Zend\InputFilter\InputFilterInterface;
-use Zend\InputFilter\OptionalInputFilter;
+use Laminas\InputFilter\Input;
+use Laminas\InputFilter\InputFilter;
+use Laminas\InputFilter\InputFilterInterface;
+use Laminas\InputFilter\OptionalInputFilter;
 use PHPUnit\Framework\TestCase;
 
 /**
- * @covers \Zend\InputFilter\OptionalInputFilter
+ * @covers \Laminas\InputFilter\OptionalInputFilter
  */
 class OptionalInputFilterTest extends TestCase
 {
