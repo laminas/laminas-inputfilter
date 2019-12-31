73e3283d9b365b02d3fdd1a11cebac9d2480e01c
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:12:26 -0600

    Rewriting as Laminas Project package

--- test/CollectionInputFilterTest.php
+++ test/CollectionInputFilterTest.php
@@ -1,25 +1,24 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-inputfilter for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-inputfilter/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-inputfilter/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\InputFilter;
+namespace LaminasTest\InputFilter;
 
+use Laminas\InputFilter\BaseInputFilter;
+use Laminas\InputFilter\CollectionInputFilter;
+use Laminas\InputFilter\Input;
+use Laminas\InputFilter\InputFilter;
+use Laminas\Validator;
 use PHPUnit_Framework_TestCase as TestCase;
-use Zend\InputFilter\BaseInputFilter;
-use Zend\InputFilter\CollectionInputFilter;
-use Zend\InputFilter\Input;
-use Zend\InputFilter\InputFilter;
-use Zend\Validator;
 
 class CollectionInputFilterTest extends TestCase
 {
     /**
-     * @var \Zend\InputFilter\CollectionInputFilter
+     * @var \Laminas\InputFilter\CollectionInputFilter
      */
     protected $filter;
 
@@ -107,12 +106,12 @@ class CollectionInputFilterTest extends TestCase
     public function testSetInputFilter()
     {
         $this->filter->setInputFilter(new BaseInputFilter());
-        $this->assertInstanceOf('Zend\InputFilter\BaseInputFilter', $this->filter->getInputFilter());
+        $this->assertInstanceOf('Laminas\InputFilter\BaseInputFilter', $this->filter->getInputFilter());
     }
 
     public function testGetDefaultInputFilter()
     {
-        $this->assertInstanceOf('Zend\InputFilter\BaseInputFilter', $this->filter->getInputFilter());
+        $this->assertInstanceOf('Laminas\InputFilter\BaseInputFilter', $this->filter->getInputFilter());
     }
 
     public function testSetCount()
