2df64b221289dfaa62078fc7efa2df6859817e6d
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:11:48 -0600

    Rewriting as Laminas Project package

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# zend-inputfilter
+# laminas-inputfilter
 
-The `Zend\InputFilter` component can be used to filter and validate generic sets
+The `Laminas\InputFilter` component can be used to filter and validate generic sets
 of input data. For instance, you could use it to filter `$_GET` or `$_POST`
 values, CLI arguments, etc.
 
-- File issues at https://github.com/zendframework/zend-inputfilter/issues
-- Documentation is at http://framework.zend.com/manual/current/en/index.html#zend-inputfilter
+- File issues at https://github.com/laminas/laminas-inputfilter/issues
+- Documentation is at https://docs.laminas.dev/laminas-inputfilter
--- src/Input.php
+++ src/Input.php
@@ -1,17 +1,16 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-inputfilter for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-inputfilter/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-inputfilter/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\InputFilter;
+namespace Laminas\InputFilter;
 
-use Zend\Filter\FilterChain;
-use Zend\Validator\ValidatorChain;
-use Zend\Validator\NotEmpty;
+use Laminas\Filter\FilterChain;
+use Laminas\Validator\NotEmpty;
+use Laminas\Validator\ValidatorChain;
 
 class Input implements InputInterface
 {
