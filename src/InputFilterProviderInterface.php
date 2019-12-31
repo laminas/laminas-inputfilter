c0e2b25df3180ff5eb0b906222bd2a6e06335207
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:11:42 -0600

    Rewriting as Laminas Project package

--- src/InputFilterProviderInterface.php
+++ src/InputFilterProviderInterface.php
@@ -1,24 +1,22 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_InputFilter
+ * @see       https://github.com/laminas/laminas-inputfilter for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-inputfilter/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-inputfilter/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\InputFilter;
+namespace Laminas\InputFilter;
 
 /**
- * @category   Zend
- * @package    Zend_InputFilter
+ * @category   Laminas
+ * @package    Laminas_InputFilter
  */
 interface InputFilterProviderInterface
 {
     /**
      * Should return an array specification compatible with
-     * {@link Zend\InputFilter\Factory::createInputFilter()}.
+     * {@link Laminas\InputFilter\Factory::createInputFilter()}.
      *
      * @return array
      */
